c30451d07a25155faf40b3369eff3eecc8e6d8da
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-06-01 21:31:50 +0300

    refactor: Rename `Method.cloneMutableAndPreserveParameters()` to `Method.cloneParameters()`

--- patch-library/src/main/kotlin/app/morphe/util/BytecodeUtils.kt
+++ patch-library/src/main/kotlin/app/morphe/util/BytecodeUtils.kt
@@ -359,7 +359,7 @@ fun Method.indexOfFirstResourceId(resourceName: String): Int {
 /**
  * Get the index of the first instruction with the id of the given resource name or throw a [PatchException].
  *
- * Requires [resourceMappingPatch] as a dependency.
+ * Requires [app.morphe.patches.all.misc.resources.resourceMappingPatch] as a dependency.
  *
  * @throws [PatchException] if the resource is not found, or the method does not contain the resource id literal value.
  * @see [indexOfFirstResourceId], [indexOfFirstLiteralInstructionReversedOrThrow]
@@ -872,8 +872,23 @@ fun BytecodePatchContext.forEachLiteralValueInstruction(
             }
         }
     }
-
 }
+
+
+@Deprecated(
+    "Method was renamed to Method.cloneParameters()",
+    replaceWith = ReplaceWith("cloneParameters()")
+)
+context(patchContext: BytecodePatchContext)
+fun Method.cloneMutableAndPreserveParameters() = cloneParameters()
+
+@Deprecated(
+    "Method was renamed to Method.cloneParameters()",
+    replaceWith = ReplaceWith("cloneParameters(mutableClass)")
+)
+context(patchContext: BytecodePatchContext)
+fun Method.cloneMutableAndPreserveParameters(mutableClass : MutableClass) = cloneParameters(mutableClass)
+
 
 /**
  * Additional registers effectively take the place of the pX parameters (p0, p1, p2, etc.)
@@ -884,7 +899,7 @@ fun BytecodePatchContext.forEachLiteralValueInstruction(
  * **Fingerprint match indexes will be increased positively by [numberOfParameterRegistersLogical]**.
  */
 context(patchContext: BytecodePatchContext)
-fun Method.cloneMutableAndPreserveParameters() = cloneMutableAndPreserveParameters(
+fun Method.cloneParameters() = cloneParameters(
     patchContext.mutableClassDefBy(definingClass)
 )
 
@@ -896,7 +911,7 @@ fun Method.cloneMutableAndPreserveParameters() = cloneMutableAndPreserveParamete
  *
  * **Fingerprint match indexes will be increased positively by [numberOfParameterRegistersLogical]**.
  */
-fun Method.cloneMutableAndPreserveParameters(mutableClass : MutableClass) : MutableMethod {
+fun Method.cloneParameters(mutableClass : MutableClass) : MutableMethod {
     check (!AccessFlags.STATIC.isSet(accessFlags) || parameters.isNotEmpty()) {
         "Static methods have no parameter registers to preserve"
     }
@@ -907,7 +922,7 @@ fun Method.cloneMutableAndPreserveParameters(mutableClass : MutableClass) : Muta
 
     // Replace existing method with cloned with more registers.
     mutableClass.methods.apply {
-        remove(this@cloneMutableAndPreserveParameters)
+        remove(this@cloneParameters)
         add(clonedMethod)
     }
 
